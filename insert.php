30af8a9b0af1c225692392491b36aa19526685bc
Anshcool5 <anshul5@ualberta.ca>
2024-01-27 14:51:03 -0700

    data insert logic

---
+++ insert.php
@@ -0,0 +1,29 @@
+<!-- insert.php -->
+<?php
+// Connect to MySQL database
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Insert data into table
+$name = $_POST["name"];
+$email = $_POST["email"];
+
+$sql = "INSERT INTO MyGuests (firstname, email) VALUES ('$name', '$email')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
